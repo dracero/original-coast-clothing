eb3aa01ee452a7d8e3e254ffde94e39f50a7b396
dracero <diego.racero@gmail.com>
2019-10-07 23:21:13 +0000

    domi10

--- services/receive.js
+++ services/receive.js
@@ -85,7 +85,7 @@ module.exports = class Receive {
       message.includes("start over")
     ) {
       //response = Response.genNuxMessage(this.user);
-      response = Curation.handlePayload("CURATION");
+      response = Order.handlePayload("TRAK_ORDER");
     } else if (Number(message)) {
       response = Order.handlePayload("ORDER_NUMBER");
     } else if (message.includes("#")) {
