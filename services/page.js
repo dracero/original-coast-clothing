19032592b1ec80be349a32586339344d310bdaff
dracero <diego.racero@gmail.com>
2019-10-08 23:20:09 +0000

    aver2

--- services/page.js
+++ services/page.js
@@ -37,7 +37,7 @@ module.exports = class TestDiego {
           Response.genText(i18n.__("order.dialog")),
           Response.genText(i18n.__("order.searching")),
           Response.genImageTemplate(
-            `${config.appUrl}/neutral-work.jpg`
+            `${config.appUrl}/styles/neutral-work.jpg`
           )
         ];
         break;
