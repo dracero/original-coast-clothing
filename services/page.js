b67b9db04c274bd3cc28a524cc194749283bef61
dracero <diego.racero@gmail.com>
2019-10-08 23:29:55 +0000

    🏹🏅 Checkpoint ./services/page.js:3726892/40

--- services/page.js
+++ services/page.js
@@ -37,7 +37,7 @@ module.exports = class TestDiego {
           Response.genText(i18n.__("order.dialog")),
           Response.genText(i18n.__("order.searching")),
           Response.genImageTemplate(
-            `${config.appUrl}/styles/neutral-work.jpg`
+            `${config.appUrl}/styles/female-dinner.jpg`
           )
         ];
         break;
